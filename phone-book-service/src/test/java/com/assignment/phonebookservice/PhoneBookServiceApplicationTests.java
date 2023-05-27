4bd5a45008939db4269ed9fe0b2157f59c0383e7
sairam <sairamvagicharla@gmail.com>
2023-05-27 17:13:36 +0530

    initial commit

---
+++ phone-book-service/src/test/java/com/assignment/phonebookservice/PhoneBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.phonebookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
